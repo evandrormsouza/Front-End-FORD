df977769781c7203196d7802956b1e4803440feb
Evandro Rocha Mendes de Souza <evandrormsouza@gmail.com>
2023-11-16 21:52:56 -0300

    trabalhando com JS

---
+++ JavaScript/Inicio/trabalhandoComDados/script01.js
@@ -0,0 +1,15 @@
+// camelCase
+var fabricanteCarro = "renault"
+var modeloCarro = 'logan'
+var precoCarro = 16000
+var anoCarro = 2018
+
+// fabricante: renault, modelo: logan, preco: 16000, ano: 2018
+
+console.log("fabricante: " + fabricanteCarro + ", modelo: " + modeloCarro +", preco: " + precoCarro + ", ano: " + anoCarro)
+
+// modeloCarro = "sandero"
+// console.log (modeloCarro)
+
+// modeloCarro = "clio"
+// console.log(modeloCarro)
