e860fa14ce7461e6f600f9416290ec20d4ff8e43
Evandro Rocha Mendes de Souza <142059988+evandrormsouza@users.noreply.github.com>
2023-11-17 21:42:23 -0300

    Atividade Cálculo da Circunferência

---
+++ JavaScript/Inicio/trabalhandoComDados/Atividade 1.js
@@ -0,0 +1,22 @@
+//**************** ATIVIDADE 01*********************
+
+// Faça um programa que calcule a circunferência (área) de um círculo,
+// utilizando conceitos que já vimos anteriormente, tais como,
+// variáveis, operadores aritméticos, concatenação, etc.
+// lembre-se da estrutura dos dados (Entrada de dados, Processamento e Saída de dados)
+// tendo em mente que o raio da circunferência é de R=16
+
+// entrada de dados
+const PI = 3.14
+const raioCirculo = 16
+
+// processamento de dados
+const resultadoRaio = PI * (raioCirculo * raioCirculo)
+
+//  saída de dados
+console.log (resultadoRaio + " cm²");
+
+
+//ou
+
+
