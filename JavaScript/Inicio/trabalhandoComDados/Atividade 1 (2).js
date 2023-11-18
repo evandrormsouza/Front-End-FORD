893f9b509a417d33013e719fa8e217fc3f285509
Evandro Rocha Mendes de Souza <142059988+evandrormsouza@users.noreply.github.com>
2023-11-17 21:40:06 -0300

    Atividade Cáculo Raio do Círculo

---
+++ JavaScript/Inicio/trabalhandoComDados/Atividade 1 (2).js
@@ -0,0 +1,18 @@
+//**************** ATIVIDADE 01*********************
+
+// Faça um programa que calcule a circunferência (área) de um círculo,
+// utilizando conceitos que já vimos anteriormente, tais como,
+// variáveis, operadores aritméticos, concatenação, etc.
+// lembre-se da estrutura dos dados (Entrada de dados, Processamento e Saída de dados)
+// tendo em mente que o raio da circunferência é de R=16
+
+// entrada de dados
+
+const raioCirculo = 16
+
+// processamento de dados
+
+const resultadoRaio = Math.PI * (raioCirculo * raioCirculo)
+
+// saída de dados
+console.log (resultadoRaio.toFixed(2) + " cm²")
